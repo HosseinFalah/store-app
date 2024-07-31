b32ace42099d1ce2c3aa7f62f0eb1353b6aff529
HosseinFalah <98707258+HosseinFalah@users.noreply.github.com>
2024-07-31 07:49:40 +0330

    faet: added populate category and teacher for courses

--- app/http/controllers/admin/course/course.controller.js
+++ app/http/controllers/admin/course/course.controller.js
@@ -12,8 +12,19 @@ class CourseController extends Controller {
             const { search } = req.query;
             let courses;
 
-            if (search) await CourseModel.find({ $text: { $search: search }}).sort({ _id: -1 });
-            else courses = await CourseModel.find({}).sort({ _id: -1 });
+            if (search) courses = await CourseModel.find({ $text: { $search: search }})
+                .populate([
+                    {path: 'category', select: { title: 1 }},
+                    {path: 'teacher', select: { first_name: 1, last_name: 1, mobile: 1, email: 1 }}
+                ])
+                .sort({ _id: -1 });
+            else courses = await CourseModel.find({})
+                .populate([
+                    {path: 'category', select: { title: 1 }},
+                    {path: 'teacher', select: { first_name: 1, last_name: 1, mobile: 1, email: 1 }}
+                ])
+                .sort({ _id: -1 });
+
             return res.status(StatusCodes.OK).json({
                 statusCode: StatusCodes.OK,
                 data: {
